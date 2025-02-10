57f9a11d95c6bb3ead2afc19054447f38621c69d
Cecilia Lopez <155802930+cecilia314@users.noreply.github.com>
2025-02-09 19:15:03 -0800

    Create Layout components (#2)
    * feat: Create Tabs top bar
    
    * feat: Create Header
    
    * feat: Create Loading spinner

--- App.js
+++ App.js
@@ -2,16 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GluestackUIProvider, VStack } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
-import CustomSelectInput from './src/components/common/CustomSelectInput';
+import Tabs from './src/components/layout/Tabs';
+import Header from './src/components/layout/Header';
+import Loading from './src/components/layout/Loading';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <GluestackUIProvider config={config}>
-        <StatusBar style="auto" />
-        <VStack marginTop="50">
-          <CustomSelectInput />
-        </VStack>
+        <Header />
+        <Tabs />
+        <Loading />
       </GluestackUIProvider>
     </SafeAreaProvider>
   );
